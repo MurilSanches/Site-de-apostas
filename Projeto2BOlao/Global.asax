a6a8df162b9a7905a4d7587d86a51b73a855fe9f
7863752434 <32243291+7863752434@users.noreply.github.com>
2019-05-17 11:18:42 -0300

    a

---
+++ Projeto2BOlao/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projeto2BOlao.MvcApplication" Language="C#" %>
---
+++ Projeto2BOlao/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projeto2BOlao.Startup))]
+namespace Projeto2BOlao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
