744f11fd6966796f4f7c482fc5570a87bb669444
7863752434 <32243291+7863752434@users.noreply.github.com>
2019-05-20 09:36:15 -0300

    a

--- ProjetoBolao/DAO/SiteContext.cs
+++ ProjetoBolao/DAO/SiteContext.cs
@@ -11,6 +11,8 @@ namespace ProjetoBolao.DAO
     {
         public DbSet<Usuario> Usuario2 { get; set; }
 
+        public DbSet<Time> Times { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=regulus.cotuca.unicamp.br;Initial Catalog=PR118187; User ID =PR118187; Password =PR118187");
---
+++ ProjetoBolao/Models/Time.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoBolao.Models
+{
+    public class Time
+    {
+        public int CodTime { get; set; }
+        
+        public string Nome { get; set; }
+
+        public string Foto { get; set; }
+
+        public int QntVitorias { get; set; }
+
+        public int QntDerrotas { get; set; }
+
+        public int QntEmpates { get; set; }
+
+        public int SaldoDeGols { get; set; }
+
+        public int Pontuacao { get; set; }
+    }
+}
